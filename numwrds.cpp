721a8dd05ae00e4e6f32b52c5877cac376fdf109
Valentyn <93194964+valikmed@users.noreply.github.com>
2021-11-08 01:25:07 +0200

    added some really great code

---
+++ numwrds.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<cstring>
+
+//“added some really great code”
+//not so good as it could be but OK
+//some configuration reading
+void main()
+{
+
+    char str[100];
+    int i, numwrds = 0;
+
+    std::cout << "Enter String : ";
+    std::cin.getline(str, 100);
+
+
+    for (int i = 0; str[i] != '\0'; i++)
+    {
+
+        if (str[i] == ' ')
+            numwrds++;
+
+    }
+
+    std::cout << "Total words = " << numwrds + 1;
+
+}
